8f44109ae7484069776033263f2c81d3ea3537ec
JoseMarquez99 <jamr1999@hotmail.com>
2021-05-12 18:40:39 -0500

    si

--- flutter_application_si/lib/main.dart
+++ flutter_application_si/lib/main.dart
@@ -5,6 +5,7 @@ import './src/pages/home_page.dart';
 import './src/routes/routes.dart';
 import './src/pages/homepage_alternativo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
  
 void main() => runApp(MyApp());
  
@@ -12,6 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en','US'),
+        const Locale('es','ES'),
+      ],
       title: 'listTile APP',
       debugShowCheckedModeBanner: false,
       //home:HomePage(),
--- flutter_application_si/lib/src/routes/routes.dart
+++ flutter_application_si/lib/src/routes/routes.dart
@@ -1,14 +1,22 @@
 import '../pages/alert_page.dart';
+import '../pages/animated_container.dart';
 import '../pages/avatar_page.dart';
 import '../pages/card_page.dart';
 import '../pages/home_page.dart';
+import '../pages/input_page.dart';
+import '../pages/slider_page.dart';
+
 import 'package:flutter/material.dart';
 
-Map <String, WidgetBuilder> getAplicationRoutes(){
-  return <String, WidgetBuilder>{
+
+Map<String, WidgetBuilder> getAplicationRoutes(){
+  return <String,WidgetBuilder>{
         '/': (BuildContext context) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
         'card': (BuildContext context) => CardPage(),
+        'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+        'inputs' : (BuildContext context) => InputPage(),
+        'slider':(BuildContext context) => SliderPage()
       };
-  }
+}
